44bdc25e046c368ea837efeb3fab7e0cda82c1b8
GenkiHirano <genki86web@gmail.com>
2023-04-09 17:30:49 +0900

    refactor: field,pos,blockをリファクタリング

--- tetrust/target/debug/deps/tetrust-22c5700a5fdb5e31.d
+++ tetrust/target/debug/deps/tetrust-22c5700a5fdb5e31.d
@@ -1,6 +1,7 @@
-/Users/genkihirano/Projects/rust-tetris/tetrust/target/debug/deps/tetrust-22c5700a5fdb5e31.rmeta: src/main.rs src/block.rs
+/Users/genkihirano/Projects/rust-tetris/tetrust/target/debug/deps/tetrust-22c5700a5fdb5e31.rmeta: src/main.rs src/block.rs src/game.rs
 
-/Users/genkihirano/Projects/rust-tetris/tetrust/target/debug/deps/tetrust-22c5700a5fdb5e31.d: src/main.rs src/block.rs
+/Users/genkihirano/Projects/rust-tetris/tetrust/target/debug/deps/tetrust-22c5700a5fdb5e31.d: src/main.rs src/block.rs src/game.rs
 
 src/main.rs:
 src/block.rs:
+src/game.rs:
--- tetrust/target/debug/deps/tetrust-6e51f378bc2553b3.d
+++ tetrust/target/debug/deps/tetrust-6e51f378bc2553b3.d
@@ -1,6 +1,7 @@
-/Users/genkihirano/Projects/rust-tetris/tetrust/target/debug/deps/tetrust-6e51f378bc2553b3.rmeta: src/main.rs src/block.rs
+/Users/genkihirano/Projects/rust-tetris/tetrust/target/debug/deps/tetrust-6e51f378bc2553b3.rmeta: src/main.rs src/block.rs src/game.rs
 
-/Users/genkihirano/Projects/rust-tetris/tetrust/target/debug/deps/tetrust-6e51f378bc2553b3.d: src/main.rs src/block.rs
+/Users/genkihirano/Projects/rust-tetris/tetrust/target/debug/deps/tetrust-6e51f378bc2553b3.d: src/main.rs src/block.rs src/game.rs
 
 src/main.rs:
 src/block.rs:
+src/game.rs:
