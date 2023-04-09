ac561734f4538aac432383af65fa070227f3455a
GenkiHirano <genki86web@gmail.com>
2023-04-09 17:11:30 +0900

    refactor: ブロックの定義を別ファイルに分割

--- tetrust/target/debug/deps/tetrust-22c5700a5fdb5e31.d
+++ tetrust/target/debug/deps/tetrust-22c5700a5fdb5e31.d
@@ -1,5 +1,6 @@
-/Users/genkihirano/Projects/rust-tetris/tetrust/target/debug/deps/tetrust-22c5700a5fdb5e31.rmeta: src/main.rs
+/Users/genkihirano/Projects/rust-tetris/tetrust/target/debug/deps/tetrust-22c5700a5fdb5e31.rmeta: src/main.rs src/block.rs
 
-/Users/genkihirano/Projects/rust-tetris/tetrust/target/debug/deps/tetrust-22c5700a5fdb5e31.d: src/main.rs
+/Users/genkihirano/Projects/rust-tetris/tetrust/target/debug/deps/tetrust-22c5700a5fdb5e31.d: src/main.rs src/block.rs
 
 src/main.rs:
+src/block.rs:
--- tetrust/target/debug/deps/tetrust-6e51f378bc2553b3.d
+++ tetrust/target/debug/deps/tetrust-6e51f378bc2553b3.d
@@ -1,5 +1,6 @@
-/Users/genkihirano/Projects/rust-tetris/tetrust/target/debug/deps/tetrust-6e51f378bc2553b3.rmeta: src/main.rs
+/Users/genkihirano/Projects/rust-tetris/tetrust/target/debug/deps/tetrust-6e51f378bc2553b3.rmeta: src/main.rs src/block.rs
 
-/Users/genkihirano/Projects/rust-tetris/tetrust/target/debug/deps/tetrust-6e51f378bc2553b3.d: src/main.rs
+/Users/genkihirano/Projects/rust-tetris/tetrust/target/debug/deps/tetrust-6e51f378bc2553b3.d: src/main.rs src/block.rs
 
 src/main.rs:
+src/block.rs:
