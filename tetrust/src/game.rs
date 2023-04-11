8051f9de9fd3eda73dcac71eeb6ca41d05441e55
GenkiHirano <genki86web@gmail.com>
2023-04-12 01:03:09 +0900

    feat: ブロックの色付け

--- tetrust/src/game.rs
+++ tetrust/src/game.rs
@@ -1,9 +1,10 @@
+use crate::block::{block_kind, block_kind::WALL as W, BlockColor, COLOR_TABLE};
 use crate::block::{BlockKind, BlockShape, BLOCKS};
 
 // フィールドサイズ
 pub const FIELD_WIDTH: usize = 11 + 2 + 2; // フィールド＋壁＋番兵
 pub const FIELD_HEIGHT: usize = 20 + 1 + 1; // フィールド＋底＋番兵
-pub type Field = [[usize; FIELD_WIDTH]; FIELD_HEIGHT];
+pub type Field = [[BlockColor; FIELD_WIDTH]; FIELD_HEIGHT];
 
 pub struct Position {
     pub x: usize,
@@ -26,27 +27,27 @@ impl Game {
     pub fn new() -> Game {
         Game {
             field: [
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, W, 0],
+                [0, W, W, W, W, W, W, W, W, W, W, W, W, W, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             ],
             pos: Position::init(),
@@ -63,8 +64,8 @@ pub fn draw(Game { field, pos, block }: &Game) {
     // 描画用フィールドにブロックの情報を書き込む
     for y in 0..4 {
         for x in 0..4 {
-            if block[y][x] == 1 {
-                field_buf[y + pos.y][x + pos.x] = 1;
+            if block[y][x] != block_kind::NONE {
+                field_buf[y + pos.y][x + pos.x] = block[y][x];
             }
         }
     }
@@ -72,11 +73,7 @@ pub fn draw(Game { field, pos, block }: &Game) {
     println!("\x1b[H"); // カーソルを先頭に移動
     for y in 0..FIELD_HEIGHT - 1 {
         for x in 1..FIELD_WIDTH - 1 {
-            if field_buf[y][x] == 1 {
-                print!("[]");
-            } else {
-                print!(" .");
-            }
+            print!("{}", COLOR_TABLE[field_buf[y][x]]);
         }
         println!();
     }
@@ -89,7 +86,8 @@ pub fn is_collision(field: &Field, pos: &Position, block: &BlockShape) -> bool {
             if y + pos.y >= FIELD_HEIGHT || x + pos.x >= FIELD_WIDTH {
                 continue;
             }
-            if field[y + pos.y][x + pos.x] & block[y][x] == 1 {
+            if block[y][x] != block_kind::NONE && field[y + pos.y][x + pos.x] != block_kind::NONE {
+                // ブロックとフィールドのどちらも何かしらのブロックがある場合は衝突している
                 return true;
             }
         }
@@ -101,8 +99,8 @@ pub fn is_collision(field: &Field, pos: &Position, block: &BlockShape) -> bool {
 pub fn fix_block(Game { field, pos, block }: &mut Game) {
     for y in 0..4 {
         for x in 0..4 {
-            if block[y][x] == 1 {
-                field[y + pos.y][x + pos.x] = 1;
+            if block[y][x] != block_kind::NONE {
+                field[y + pos.y][x + pos.x] = block[y][x];
             }
         }
     }
